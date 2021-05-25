605e35071933118ad129b1fadd955b18c4695e8a
Taras <pentsko.t@gmail.com>
2021-05-25 22:30:57 +0300

    testing_dictionarys_methods is WORKING

--- testing_dictionarys_methods.rb
+++ testing_dictionarys_methods.rb
@@ -1,20 +1,18 @@
-
 def chose_item
-while user_chose_item = gets.chomp
-if user_chose_item == "1"
-        File.open("First.txt", "r") do |file|
-          puts read_file = file.readlines
-        end
-      elsif user_chose_item == "2"
-        File.open("Second.txt", "r") do |file|
-          puts read_file = file.readlines
-        end
-      else
-        puts "repeat"
-
+  while user_chose_item = gets.chomp
+    if user_chose_item == "1"
+      File.open("First.txt", "r") do |file|
+        puts read_file = file.readlines
       end
+    elsif user_chose_item == "2"
+      File.open("Second.txt", "r") do |file|
+        puts read_file = file.readlines
+      end
+    else
+      puts "repeat"
+    end
+  end
 end
-
 
 chose_item
 
